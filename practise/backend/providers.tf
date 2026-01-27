71e863765d85d44ffc310342d5f18c04e2d6097b
Maheshbharambe45 <bharambemahesh7@gmail.com>
2026-01-28 00:07:42 +0530

    Updated .tf files

--- practise/backend/providers.tf
+++ practise/backend/providers.tf
@@ -5,10 +5,12 @@ terraform {
       version = "6.18.0"
     }
   }
+
   backend "s3" {
     bucket = "heyyy-seriiii"
     key = "terraform.tfstate"
     region = "ap-south-1"
+    use_lockfile = true
   }
 }
 
