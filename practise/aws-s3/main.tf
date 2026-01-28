99ed843d63dc04fcfd2b418bab94c6381a1e1c2e
Maheshbharambe45 <bharambemahesh7@gmail.com>
2026-01-28 10:37:57 +0530

    Updated .tf files

--- practise/aws-s3/main.tf
+++ practise/aws-s3/main.tf
@@ -1,14 +1,21 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.Bucket_name
-
   tags = {
     Name = var.Tag_name
   }
+  
+  lifecycle {
+    prevent_destroy = true
+    create_before_destroy = true
+  }
+
 }
 
 resource "aws_s3_object" "this" {
   bucket = aws_s3_bucket.this.id
   key    = "app.js"
   source = "./app.js"
+  depends_on = [ aws_s3_bucket.this ]
 }
+
 
